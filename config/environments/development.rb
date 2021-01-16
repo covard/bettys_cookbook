aa091eadf66e54086655dcffb02a1de4b3906030
Curtis Ovard <ovardcj@gmail.com>
2021-01-16 10:37:31 -0700

    add connection for local db, set perform deliveries to true

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -54,7 +54,12 @@ Rails.application.configure do
   config.assets.quiet = true
 
   config.action_mailer.delivery_method = :letter_opener_web
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
+
+  console do
+    ActiveRecord::Base.connection
+  end
 
   # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
